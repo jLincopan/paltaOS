d89d0ea81cdfb6dd5db5ec67430aa52a396f31dc
Javier Lincopán <cocolilo148@gmail.com>
2021-03-10 10:08:43 -0300

    añadido soporte para saltos de línea, hasta ahora cuando se intentaba imprimir '\n' en pantalla salía un caracter medio raro

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -36,6 +36,17 @@ static uint8_t color_fondo;
 static uint16_t posicion_cursor = 0;
 static uint16_t* terminal_buffer;
 
+/*mueve el cursor a la posición x (recordar que esto es un array
+  donde 80 elementos seguidos representan una fila)
+  xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+  fila 1   fila 2   fila 3   fila 4   etc*/
+void terminal_moverCursor(uint16_t posicion) {
+	outb(FB_PUERTO_COMANDOS, FB_HIGH_BYTE_COMMAND);
+	outb(FB_PUERTO_DATOS,    ((posicion >> 8) & 0x00FF));
+	outb(FB_PUERTO_COMANDOS, FB_LOW_BYTE_COMMAND);
+	outb(FB_PUERTO_DATOS,    posicion & 0x00FF);
+}
+
 /*inicializa la terminal, llenándola de espacios vacíos con el
  color de fondo especificado en color_fondo*/ 
 void terminal_inicializar(void) {
@@ -68,23 +79,20 @@ void terminal_caracterXY(unsigned char c, uint8_t color, size_t x, size_t y) {
   llama a la función que posiciona realmente los carácteres*/
 void terminal_posicionarCaracter(char c) {
 	unsigned char uc = c;
-	terminal_caracterXY(uc, color_fondo, columna, fila);
-	if (++columna == VGA_ANCHO) {
-		columna = 0;
-		if (++fila == VGA_ALTO)
-			fila = 0;
-	}
-}
 
-/*mueve el cursor a la posición x (recordar que esto es un array
-  donde 80 elementos seguidos representan una fila)
-  xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
-  fila 1   fila 2   fila 3   fila 4   etc*/
-void terminal_moverCursor(uint16_t posicion) {
-	outb(FB_PUERTO_COMANDOS, FB_HIGH_BYTE_COMMAND);
-	outb(FB_PUERTO_DATOS,    ((posicion >> 8) & 0x00FF));
-	outb(FB_PUERTO_COMANDOS, FB_LOW_BYTE_COMMAND);
-	outb(FB_PUERTO_DATOS,    posicion & 0x00FF);
+	if (uc == '\n') {
+		fila++;
+		posicion_cursor = posicion_cursor - columna + 80 - 1;
+		columna = 0;
+		terminal_moverCursor(posicion_cursor);
+	} else {
+		terminal_caracterXY(uc, color_fondo, columna, fila);
+		if (++columna == VGA_ANCHO) {
+			columna = 0;
+			if (++fila == VGA_ALTO)
+				fila = 0;
+		}
+	}
 }
 
 void terminal_imprimirCadena(const char* data, size_t size) {
