d5ed5ea29e741d08eede1ed3ed7e3dbac1a1503d
Javier Lincopán <j.lincopan01@gmail.com>
2022-07-26 11:09:39 -0400

    Añadida función que imprime un solo caracter. Añadidas etiquetas para los saltos condicionales en imprimir_cadena

--- src/imprimir_cadena.asm
+++ src/imprimir_cadena.asm
@@ -1,16 +1,30 @@
+imprimir_caracter:
+	push ax
+	push bp
+	mov bp, sp
+
+	mov ah, 0x0e
+	mov al, [bp+6]
+	int 0x10
+	
+	pop ax
+	pop bp
+	ret
+
 ;;;Imprime en pantalla lo que esté en bx
 imprimir_cadena:
     pusha           ; almacenando los valores en los registros en el stack
-
-imprimir_caracter:
+	mov ah, 0x0e 	; int 10/ ah 0x0e BIOS telepype output
+	
+	.INICIO_BUCLE:
 	mov al, [bx]    ; movemos el primer caracter en bx hacia al
 	cmp al, 0       ; comparamos el caracter con el número 0
-	je fin_funcion  ; si lo que hay en al es igual a 0, saltamos a la sección
-		            ; fin_funcion
+	je .FIN_BUCLE   ; si lo que hay en al es igual a 0, saltamos a la sección
+		        	; fin_funcion ya que termina la cadena
 	int 0x10
-	add bx, 1 ; nos movemos un byte hacia adelante en memoria 
-	jmp imprimir_caracter ; volvemos al principio de la "función"
+	add bx, 1 		; nos movemos un byte hacia adelante en memoria 
+	jmp .INICIO_BUCLE ; volvemos al principio de la "función"
 
-fin_funcion:
+	.FIN_BUCLE:
     popa            ; restauramos los valores anteriores
     ret
--- src/kernel.asm
+++ src/kernel.asm
@@ -1,15 +1,16 @@
 	;Seteando modo de video
 	mov ah, 0x00
 	mov al, 0x03
-
 	int 0x10
 
-	mov ah, 0x0e 	; int 10/ ah 0x0e BIOS telepype output
-	mov bx, cadena 	; cargando la dirección de memoria en que se
-			     	; encuentra "cadena" al registro bx
+	mov bx, cadena 	; cargando la cadena en memoria
 	call imprimir_cadena
 	mov bx, cadena2
 	call imprimir_cadena
+	push 'x'
+	call imprimir_caracter
+	push 'D'
+	call imprimir_caracter
 	hlt
 	
     cadena: db 'hola k ase1', 0xa, 0xd, 0
